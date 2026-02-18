510162d72e41222876b9f511460f639c31913ea1
harshad bhavake <harshadbhavake77@gmail.com>
2026-02-18 23:59:21 +0530

    Update get_connection to use DATABASE_URL
    Refactor database connection to use DATABASE_URL.

--- backend/db.py
+++ backend/db.py
@@ -1,20 +1,10 @@
 import psycopg2
 import os
-from dotenv import load_dotenv
-
-# Load .env once
-load_dotenv()
 
 def get_connection():
-    required = ["DBPORT", "DBNAME", "DB.USER", "DB_PASSWORD"]
-    for var in required:
-        if not os.getenv(var):
-            raise RuntimeError(f"Missing env variable: {var}")
+    database_url = os.getenv("DATABASE_URL")
 
-    return psycopg2.connect(
-        port=os.getenv("DBPORT"),
-        dbname=os.getenv("DBNAME"),
-        user=os.getenv("DB.USER"),
-        password=os.getenv("DB_PASSWORD"),
-    )
+    if not database_url:
+        raise RuntimeError("Missing env variable: DATABASE_URL")
 
+    return psycopg2.connect(database_url)
