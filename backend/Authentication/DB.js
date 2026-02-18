e7307a78f02a4d361391e669c2e4d54164809460
harshad bhavake <harshadbhavake77@gmail.com>
2026-02-18 12:08:08 +0530

    Remove console log for DATABASE_URL
    Removed console log for DATABASE_URL in DB.js.

--- backend/Authentication/DB.js
+++ backend/Authentication/DB.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    console.log(DATABASE_URL),
     ssl:{
         rejectUnauthorized: false
     },
